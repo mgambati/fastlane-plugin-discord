d8924f94559b05c0b41eb06f52cb6e2a72b3ca6a
Matheus Gambati <matheusgambati@gmail.com>
2019-01-13 17:54:37 -0300

    Added tests

--- spec/discord_action_spec.rb
+++ spec/discord_action_spec.rb
@@ -1,10 +1,17 @@
 describe Fastlane::Actions::DiscordAction do
   describe '#run' do
-    it 'Send notification' do
+    it 'Send notification about itunes' do
       expect(Fastlane::UI).to receive(:message).with("Notifying Discord")
-      WEBHOOK_URL = 'Discord Hook Here'
+      WEBHOOK_URL = ENV['DISCORD_TEST_WEBHOOK']
 
-      Fastlane::Actions::DiscordAction.run(store: 'ios', app_name: 'IMEA', build_number: 29, version: '2.0.18', url: WEBHOOK_URL)
+      Fastlane::Actions::DiscordAction.run(store: 'ios', app_name: 'IMEA', build_number: 1, version: '1.0.0', url: WEBHOOK_URL)
+    end
+
+    it 'Send notification about google play' do
+      expect(Fastlane::UI).to receive(:message).with("Notifying Discord")
+      WEBHOOK_URL = ENV['DISCORD_TEST_WEBHOOK']
+
+      Fastlane::Actions::DiscordAction.run(app_name: 'IMEA', build_number: 1, version: '1.0.0', url: WEBHOOK_URL)
     end
   end
 end
